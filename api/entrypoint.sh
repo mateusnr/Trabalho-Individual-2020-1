2470fb07fa217f3ab6f8e8ca0f113d4e3ec99a37
Mateus Nóbrega <mateus@nobrega.dev>
2020-12-11 23:10:29 -0300

    Dockerize database and backend

---
+++ api/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -rf /opt/app/tmp/pids/server.pid
+
+rake db:create
+rake db:migrate
+
+if [ "$RAILS_ENV" = "development" ]
+then
+    rails server -p 3000 -b 0.0.0.0
+elif [ "$RAILS_ENV" = "test" ]
+then
+    rake test
+fi
